9bdd570016c8929d04db70cdb3a102ecdd6b0d71
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2011-10-25 16:17:32 +1300

    Add "pluginname" string for Moodle.org plugins database

--- lang/en/assignment_simpleuploadsingle.php
+++ lang/en/assignment_simpleuploadsingle.php
@@ -1,5 +1,6 @@
 <?php
 
+$string['pluginname'] = 'Simple Upload (Single)';
 $string['typesimpleuploadsingle'] = 'Simple Upload (Single)';
 $string['upload'] = 'Upload this file';
 $string['advanceduploadafile'] = 'Advanced Upload';
